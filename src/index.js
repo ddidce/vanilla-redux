c7d029bbc3585b0235da77bf02dce8fff236ff09
ko <ko>
2020-11-10 01:24:23 +0900

    action

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/** @format */
+
 // store는 나의 데이터 즉 state를 담는 곳
 import { createStore } from "redux";
 
@@ -7,9 +9,23 @@ const number = document.querySelector("span");
 
 //리듀서는 무조건 함수여야함
 //reducer이 return하는 건 전부 data가 된다.
-const countModifier = (state = 0) => {
-  return state;
+// action은 redux에서 함수를 부를때 쓰는 두번째 parameter or argument이다
+const countModifier = (count = 0, action) => {
+  console.log("count =>", count + "action => ", action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  }
+  return count;
 };
 //스토어를 만들면 리듀서를 달라고 요청함
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
 console.log(countStore.getState());
+
+//스토어를 통해 메시지를 보내려면 dispatch를 사용하면 된다. 그리고
+//전송한 값을 action에 넣고 action을 체크해보면 된다.
